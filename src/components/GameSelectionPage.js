d1391caee6fe12c16184f13224fbd27628cc3d99
Biter <flipbit@mailfence.com>
2023-12-14 15:31:40 -0800

    new changes

--- src/components/GameSelectionPage.js
+++ src/components/GameSelectionPage.js
@@ -1,41 +1,4 @@
 // GameSelectionPage.js
-// import React, { useState } from 'react';
-
-// const GameSelectionPage = ({ onSelectGame }) => {
-//   const [selectedGame, setSelectedGame] = useState('');
-
-//   const handleGameSelect = () => {
-//     if (selectedGame === 'ticTacToe') {
-//       onSelectGame('ticTacToe');
-//     }
-//     // Add more games as needed
-
-//     // If you add more games, you can handle them similarly, e.g., onSelectGame('anotherGame');
-//   };
-
-//   return (
-//     <div>
-//       <h1>Choose a Game</h1>
-//       <div>
-//         <label>
-//           <input
-//             type="radio"
-//             value="ticTacToe"
-//             checked={selectedGame === 'ticTacToe'}
-//             onChange={() => setSelectedGame('ticTacToe')}
-//           />
-//           Tic Tac Toe
-//         </label>
-//         {/* Add more game options as needed */}
-//       </div>
-//       <button onClick={handleGameSelect}>Start Game</button>
-//     </div>
-//   );
-// };
-
-// export default GameSelectionPage;
-
-// GameSelectionPage.js
 import React from 'react';
 
 const GameSelectionPage = ({ onSelectGame }) => {
